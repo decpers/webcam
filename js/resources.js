cfc3161ccf2d6b3c112fabe124b4325d2c024358
Decpers Development <71205293+decpers@users.noreply.github.com>
2021-03-02 10:57:38 -0500

    Se mejoró la documentación

--- js/resources.js
+++ js/resources.js
@@ -1,55 +1,18 @@
-
+//Sirve para no tener que escribir tanto caracteres en nuestro código 
 export const d = document;
+//Esta variable sirve para delagación de evento
 export const body = d.getElementById('body');
+//
 const btn = d.getElementById('btn-actions');
+//Caja que muestra mensaje cuando la cámara está apagada o en pause
 const message = d.getElementById('message');
 
+//Este objeto indica el estado de la cámara web
 let obj = {
-    value: true,
     active: false
 }
 
-
-
-export const newWebCam = (obj) => {
-    navigator.mediaDevices.getUserMedia({
-        video: {
-             width:200, height: 200
-        }
-    }).then((stream)=> {
-        
-        let webCam = d.getElementById('webcam');
-
-        webCam.srcObject =  stream;
-        if(obj.active === true){
-            btn.innerHTML = 'Desactivar'
-            webCam.classList.remove('d-none')
-            message.classList.add('d-none')
-            btn.classList.add('btn-red')
-            webCam.play();
-            
-        }else{
-            btn.innerHTML = 'Activar'
-            webCam.classList.add('d-none')
-            message.classList.remove('d-none')
-            btn.classList.remove('btn-red')
-            webCam.pause();  
-        }
-        
-        
-
-        
-        
-
-    
-    })
-
-    .catch(error => {
-        console.log(error);
-    })
-    
-}
-
+//Esta función sirve de interruptor
 export const clickBtn = (e) => {
     if(e.target.classList.contains('btn-actions')){
         if(obj.active === false){
@@ -61,3 +24,38 @@ export const clickBtn = (e) => {
         }
     }
 }
+
+//Esta función activa o desactiva la cámara dependiendo del estado
+export const newWebCam = (obj) => {
+    navigator.mediaDevices.getUserMedia({
+        video: {
+            //Establecemos las medidas de la pantalla de la cámara
+             width:200, height: 200
+        }
+    }).then((stream)=> {
+        
+        let webCam = d.getElementById('webcam');
+        webCam.srcObject =  stream;
+        //Activamos y mostramos lo que está capturando la cámara
+        if(obj.active === true){
+            btn.innerHTML = 'Desactivar'
+            webCam.classList.remove('d-none')
+            message.classList.add('d-none')
+            btn.classList.add('btn-red')
+            webCam.play();
+            
+        }else{
+            //Pausamos y ocultamos lo que está capturando la cámara
+            btn.innerHTML = 'Activar'
+            webCam.classList.add('d-none')
+            message.classList.remove('d-none')
+            btn.classList.remove('btn-red')
+            webCam.pause();  
+        }      
+    })
+
+    .catch(error => {
+        console.log(error);
+    })    
+}
+
