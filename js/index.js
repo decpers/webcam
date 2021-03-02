df14eed1c771fba6dc15f84e7de992705c88c250
Decpers Development <71205293+decpers@users.noreply.github.com>
2021-03-02 11:00:03 -0500

    Se mejoró la documentación

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 import {d, body, newWebCam, clickBtn} from './resources.js';
 
-
-
-
-
+//Delegamos eventos
 d.addEventListener('DOMContentLoaded', () =>{
     body.addEventListener('click', (e) => { 
         clickBtn(e);   
     })
-
-
 })
